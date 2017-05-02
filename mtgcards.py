8ef179da9e2aa369c5ab89ab3d1692cb61431cae
Josh Robinson <josh.robinson@gmail.com>
2017-05-02 14:12:35 -0400

    a bit closer to getting a request to list out results

--- mtgcards.py
+++ mtgcards.py
@@ -1,3 +1,14 @@
+from __future__ import print_function
 import requests
+import json
 
-requests.get()
+mtgResponse = requests.get(
+    'https://api.magicthegathering.io/v1/cards?gameFormat=Modern&text=opponent,damage,discards',
+    headers={"Accept": "application/json"})
+
+cards = json.loads(bytes(bytearray(mtgResponse.text, encoding='utf-8')))
+
+for card in cards:
+    print("card name: {}, mana cost: {}, type: {}, text: {}"
+          .format(card.name, card.manaCost, card.type, card.text))
+
