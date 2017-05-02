dc5427094ab421016e4ee42f24df2dd68ca1ffc6
Josh Robinson <josh.robinson@gmail.com>
2017-05-02 17:05:53 -0400

    a bit more worky worky

--- mtgcards.py
+++ mtgcards.py
@@ -6,9 +6,9 @@ mtgResponse = requests.get(
     'https://api.magicthegathering.io/v1/cards?gameFormat=Modern&text=opponent,damage,discards',
     headers={"Accept": "application/json"})
 
-cards = json.loads(bytes(bytearray(mtgResponse.text, encoding='utf-8')))
+cardCollection = json.loads(mtgResponse.text)
 
-for card in cards:
-    print("card name: {}, mana cost: {}, type: {}, text: {}"
-          .format(card.name, card.manaCost, card.type, card.text))
+for card in cardCollection['cards']:
+    print("card name: {}, mana cost: {}, type: {}"
+          .format(card['name'], card['manaCost'], card['type']))
 
