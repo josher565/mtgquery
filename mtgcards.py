298dbb1e992dcc0401d6b28d37c1092b90ba3e7b
Josh Robinson <josh.robinson@gmail.com>
2017-05-03 10:18:58 -0400

    working call and print to console

--- mtgcards.py
+++ mtgcards.py
@@ -1,6 +1,8 @@
 from __future__ import print_function
 import requests
 import json
+import codecs
+import sys
 
 mtgResponse = requests.get(
     'https://api.magicthegathering.io/v1/cards?gameFormat=Modern&text=opponent,damage,discards',
@@ -8,7 +10,10 @@ mtgResponse = requests.get(
 
 cardCollection = json.loads(mtgResponse.text)
 
-for card in cardCollection['cards']:
-    print("card name: {}"
-          .format(card['name']))
 
+UTF8Writer = codecs.getwriter('utf8')
+sys.stdout = UTF8Writer(sys.stdout)
+
+for card in cardCollection['cards']:
+    print(u"\n\ncard name: {}, mana cost: {}, type: {}, \ntext: {}"
+          .format(card['name'], card['manaCost'], card['type'], card['text']))
